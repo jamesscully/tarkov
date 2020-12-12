28ef6ce25578741732a7237f4c5e57cd51724dac
James Scully <james.will.scully@gmail.com>
2020-12-12 11:34:02 +0000

    Rename main form, map stretches to match window

---
+++ TarkovMain.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace TarkovAssistant
+{
+    partial class TarkovMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.picMap = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picMap)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(800, 52);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Map Selection";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(88, 19);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Shoreline";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.MapButtonClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Customs";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.MapButtonClick);
+            // 
+            // picMap
+            // 
+            this.picMap.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.picMap.Location = new System.Drawing.Point(0, 52);
+            this.picMap.Name = "picMap";
+            this.picMap.Size = new System.Drawing.Size(800, 398);
+            this.picMap.TabIndex = 2;
+            this.picMap.TabStop = false;
+            this.picMap.MouseDown += new System.Windows.Forms.MouseEventHandler(this.OnMouseDown);
+            this.picMap.MouseUp += new System.Windows.Forms.MouseEventHandler(this.OnMouseUp);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(169, 19);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Interchange";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.MapButtonClick);
+            // 
+            // TarkovMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.picMap);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "TarkovMain";
+            this.ShowIcon = false;
+            this.Text = "Tarkov Assistant - Main";
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.picMap)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox picMap;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ TarkovMain.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+using TarkovAssistant.Properties;
+
+namespace TarkovAssistant
+{
+    public partial class TarkovMain : Form
+    {
+        private Bitmap currentMap = null;
+
+        public TarkovMain()
+        {
+            InitializeComponent();
+
+            // load default map
+            LoadMapImage(Resources.customs);
+
+        }
+
+        private void LoadMapImage(Image img)
+        {
+            
+            if (picMap.Image != null)
+            {
+                // free image resource if we already assigned one
+                picMap.Image.Dispose();
+            }
+
+            picMap.SizeMode = PictureBoxSizeMode.Zoom;
+            picMap.Image = img;
+
+            currentMap = new Bitmap(img);
+        }
+
+        private void MapButtonClick(object sender, EventArgs e)
+        {
+            Button button = (Button) sender;
+
+            string mapName = button.Text.ToLower();
+
+            Bitmap bmp = null;
+
+            try
+            {
+                bmp = (Bitmap) Resources.ResourceManager.GetObject(mapName);
+            }
+            catch (Exception)
+            {
+                // load customs map if the map name is not found
+                Debug.WriteLine("Error retrieving resource (name): " + mapName);
+                bmp = Resources.customs;
+            }
+
+            LoadMapImage(bmp);
+        }
+
+        private void OnMouseDown(object sender, MouseEventArgs e)
+        {
+            Debug.WriteLine($"Mouse down at ({e.X}, {e.Y})");
+        }
+
+        private void OnMouseUp(object sender, MouseEventArgs e)
+        {
+            Debug.WriteLine($"Mouse up at ({e.X}, {e.Y})");
+        }
+    }
+}
