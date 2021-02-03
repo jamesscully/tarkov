fcfc8e5bea64a8a69c3e06240bacf29f1ffc88e3
James Scully <james.will.scully@gmail.com>
2021-02-03 15:52:29 +0000

    Update scripts into build and build + deploy

---
+++ scripts/BuildAndDeploy.ps1
@@ -0,0 +1,30 @@
+﻿$BUCKET_NAME = 'jwscully.uk'
+
+# call our build-script
+./BuildScript.ps1
+
+if(!$?) {
+	echo "Error occured during build phase"
+}
+
+# move to the output of our C# script
+cd ../updates/
+
+
+# for each of the files (zip, xml) we want to move these to AWS
+$files = Get-ChildItem "." -Filter *.*
+
+echo "Found files: $files"
+
+foreach ($f in $files) {
+	
+	# Upload file (requires my AWS tokens)
+	Write-S3Object -BucketName "$BUCKET_NAME" -Key "tarkov-assistant/$f" -File "$f"
+	
+	if($?) {
+		echo "Successfully uploaded $f to AWS"
+	}
+}
+
+# return to original place of execution
+cd ../scripts/
--- scripts/BuildScript.ps1
+++ scripts/BuildScript.ps1
@@ -1,36 +1,18 @@
-$REF_FS = '-reference:"C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.5\System.IO.Compression.FileSystem.dll"'
+# This file will build the project and our compile our GenerateUpdateScript
+# GenerateUpdateScript will generate the .XML and .ZIP files needed for the auto-updater
 
-$BUCKET_NAME = 'jwscully.uk'
+$REF_FS = '-reference:"C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.5\System.IO.Compression.FileSystem.dll"'
 
 $XML_PATH = 'C:\Users\yames\source\repos\TarkovAssistantWPF\updates\update_info.xml'
 $ZIP_PATH = '../updates/update.zip'
 
+
+# Compile our project
+MSBuild ../TarkovAssistantWPF.csproj
+
 # Compile and run our update files script
 csc -reference:"$REF_FS" .\GenerateUpdateFiles.cs; .\GenerateUpdateFiles.exe
 
-echo "Moving to AWS..."
-
-# move into the output of our C# script
-cd ../updates/
-
-
-# for each of the files (zip, xml) we want to move these to AWS
-$files = Get-ChildItem "." -Filter *.*
-
-echo "Found files: $files"
-
-foreach ($f in $files) {
-	
-	# Upload file (requires my AWS tokens)
-	Write-S3Object -BucketName "$BUCKET_NAME" -Key "tarkov-assistant/$f" -File "$f"
-	
-	if($?) {
-		echo "Successfully uploaded $f to AWS"
-	}
-}
-
-# return to original place of execution
-cd ../scripts/
 
 
 
